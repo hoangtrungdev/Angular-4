dfb84eecca2ae32b9c01c280964b21318665ce2c
ubuntu <trung.hoang@chudu24.com>
2017-08-17 19:11:28 +0700

    update code

--- src/app/default/detail/detail.module.ts
+++ src/app/default/detail/detail.module.ts
@@ -4,17 +4,30 @@ import { CommonModule } from '@angular/common';
 import { DetailRoutingModule } from './detail-routing.module';
 import { DetailComponent } from './detail.component';
 import { LoadingModule } from '../../shared';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer} from '@angular/platform-browser';
+
+@Pipe({ name: 'safe' })
+export class SafePipe implements PipeTransform {
+    constructor(private sanitizer: DomSanitizer) {}
+    transform(url) {
+        return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+    }
+}
 
 
 @NgModule({
-  imports: [
-    CommonModule,
-    DetailRoutingModule,
-      LoadingModule
-  ],
-  declarations: [
-      DetailComponent
-  ]
+    imports: [
+        CommonModule,
+        DetailRoutingModule,
+        LoadingModule,
+        NgbModule.forRoot()
+    ],
+    declarations: [
+        DetailComponent,
+        SafePipe
+    ]
 })
 export class DetailModule { }
